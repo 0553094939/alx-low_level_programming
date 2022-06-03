d2b2256436f0fbf67838c2b17b4ff612f0bd1d73
0553094939 <emmanuelbuabeng222@gmail.com>
2022-06-03 14:10:13 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - reverse
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letters--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0)
+}
