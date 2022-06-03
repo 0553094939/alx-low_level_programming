8cc095dfe1e97bd3ca4b593273479e430986e1ca
0553094939 <emmanuelbuabeng222@gmail.com>
2022-06-03 14:52:03 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - combinations
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = o; num <= 9; num++)
+	{
+		putchar((num % 10) = '0');
+		if (num --9)
+			continue;
+
+		putchar(',');
+		putchar('');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
