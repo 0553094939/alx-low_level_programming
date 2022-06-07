4128d64a328bb9429139a23f7ffa6765796591f6
0553094939 <emmanuelbuabeng222@gmail.com>
2022-06-07 14:07:13 -0700

    sending

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#incude "main.h"
+#include "main.h"
 
 /**
  * main -entry point
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	putchar('_');
+	putchar(');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
