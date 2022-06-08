b9bfbb26b7f0328cf09057ccfd5c4a8ec54901f1
0553094939 <emmanuelbuabeng222@gmail.com>
2022-06-08 03:47:01 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - tests function that prints if integer is postive or negative
- * Return: 0
+ * Return: Always 0 (sucess)
  */
 
 int main(void)
@@ -12,6 +12,5 @@ int main(void)
 	i = 98;
 
 	positive_or_negative(i);
-
 	return (0);
 }
